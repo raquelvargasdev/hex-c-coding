9324c37a9d4c0dba7ed4c4b042e0220b0762ace2
vargasraquel <vargasraquel@gmail.com>
2021-04-12 20:55:36 +0000

    agora vai

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/* printing without using printf */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
