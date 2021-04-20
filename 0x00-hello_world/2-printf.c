5fee79a149a9710324d5302891029c7017bcbe81
vargasraquel <vargasraquel@gmail.com>
2021-04-20 16:30:41 +0000

    testando betty

--- 0x00-hello_world/2-printf.c
+++ 0x00-hello_world/2-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main: prints "with proper grammar but the outcome is a piece of art,"
- * Return: 0
- */
+* main: prints "with proper grammar but the outcome is a piece of art,"
+* Return: 0
+*/
 
 int main(void)
 {
