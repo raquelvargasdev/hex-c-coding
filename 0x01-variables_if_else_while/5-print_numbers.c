3d2d268a612a8e5a16139dbdac40347edbb43081
vargasraquel <raquelvargas@gmail.com>
2021-04-21 18:37:05 -0300

    adicionando 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 10
+ * Return: returns always 0
+ */
+
+int main(void)
+{
+	char number;
+	
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+
+	return (0);
+}
